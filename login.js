60231fde114b2e82257641fb2145474361e6e0ea
indradev <yadavdevendra99876@gmail.com>
2020-07-15 22:48:03 +0530

    login working completed

--- login.js
+++ login.js
@@ -5,13 +5,22 @@ app.controller("login", function($scope ,$http){
         // console.log(data);
         $http({
             method:"POST",
-            url:"http://5dc47170c62f.ngrok.io/login/",
+            url:"http://5117794dcd51.ngrok.io/login/",
             data:{
                 "lib_id":$scope.lib_id,
                 "password":$scope.password
             }
         }).then(function mySuccess(response) {
-            console.log(response)
-          })
+            console.log(response.data[0]);
+            if (response.data == "Invalid Credentials"){
+                document.getElementById("out_resp").innerHTML="OPPS! wrong password"
 
+            }
+            else {
+            var value=JSON.stringify(response.data)
+            localStorage.setItem("datatech",value)
+            window.location.href="teacher/teacherds.html"
+            }
+        });
     }})
+    
